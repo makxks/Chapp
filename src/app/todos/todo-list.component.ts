2c1bd968370c05ad47b38be1af2bc2e08aff775c
Maximilian G. Platt <maxplatt7@gmail.com>
2018-01-22 18:11:04 +0800

    add more todos to test

--- src/app/todos/todo-list.component.ts
+++ src/app/todos/todo-list.component.ts
@@ -22,9 +22,29 @@ export class TodoListComponent {
 		group: 'group1'
 	};
 
+	todo2: Todo = {
+		name: 'another todo',
+		description: 'to test groups',
+		user: 'Max',
+		deadline: 'later',
+		group: 'group2'
+	}
+
+	todo3: Todo = {
+		name: 'a third todo',
+		description: 'this is todo3',
+		user: 'Max',
+		deadline: 'now',
+		group: 'group1'
+	};
+
 	ngOnInit(){
 		if(this.groupname == 'group1'){
 			this.todos.push(this.todo);
+			this.todos.push(this.todo3);
+		}
+		if(this.groupname == 'group2'){
+			this.todos.push(this.todo2);
 		}
 	}
 }
